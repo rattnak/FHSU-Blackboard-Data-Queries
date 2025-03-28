1a719fd9ecb48580a940d135ae0142492217f0e7
Chanrattnak Mong <2023002mong@aupp.edu.kh>
2025-03-28 12:44:47 -0500

    instructor is included

--- ultra-adoption-since-date/query.sql
+++ ultra-adoption-since-date/query.sql
@@ -8,8 +8,14 @@ SELECT
         WHEN lc.design_mode = 'C' THEN 'Classic'
         WHEN lc.design_mode IN ('U', 'P') THEN 'Ultra'
         ELSE 'Unknown'
-    END AS course_type
+    END AS course_type,
+    CONCAT(p.first_name, ' ', p.last_name) AS instructor_name  -- Combine first and last name for instructor
 FROM cdm_lms.course lc
 INNER JOIN cdm_lms.term lt ON lt.id = lc.term_id
+INNER JOIN cdm_lms.person_course lpc  -- Join person_course to filter by course_role
+    ON lpc.course_id = lc.id 
+    AND lpc.course_role = 'I'  -- Only instructors
+INNER JOIN cdm_lms.person p  -- Join person to get first and last name
+    ON p.id = lpc.person_id
 WHERE lt.start_date > '2024-08-01'
 ORDER BY lt.start_date, lc.name;
