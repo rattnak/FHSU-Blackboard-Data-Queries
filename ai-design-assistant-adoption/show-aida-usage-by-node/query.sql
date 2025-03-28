be95bfd6bc87e434068bb2ec51a0533b18ffb08b
Chanrattnak Mong <2023002mong@aupp.edu.kh>
2025-03-28 13:42:45 -0500

    Show AIDA usage by Node

---
+++ ai-design-assistant-adoption/show-aida-usage-by-node/query.sql
@@ -0,0 +1,35 @@
+-- PURPOSE: SHOW AIDA USAGE BY NODE
+
+with ai_summary as (
+select
+    ifnull(ih.hierarchy_name_seq, 'No Node') as ih_node,
+    ifnull(lt.name, 'No Term') as term,
+    lci.course_id,
+    lc.name as course_name,
+    lc.course_number,
+    lci.id as course_item_id,
+    lci.item_type,
+    lci.name,
+    lci.stage,
+    lci.ai_status,
+    concat(lp.first_name,' ',lp.last_name) as creator,
+    lci.created_time,
+    lci.modified_time,
+    --datediff(second,lci.created_time,lci.modified_time) as modified_x_seconds_after_creation,
+    case when datediff(second,lci.created_time,lci.modified_time) > 10 then 'Modified' else 'Unmodified' end as modified_indicator
+from cdm_lms.course_item lci
+inner join cdm_lms.person lp
+    on lp.id = lci.person_id
+inner join cdm_lms.course lc
+    on lc.id = lci.course_id
+left join cdm_lms.institution_hierarchy_course ihc
+    on ihc.course_id = lc.id
+    And ihc.primary_ind = 1
+left join cdm_lms.institution_hierarchy ih
+    on ihc.institution_hierarchy_id = ih.id
+left join cdm_lms.term lt
+    on lt.id = lc.term_id
+)
+
+
+select ih_node, count(distinct course_id) as course_count, count(course_item_id) as item_count from ai_summary where ai_status = 'Y' group by 1 order by 3 desc 
