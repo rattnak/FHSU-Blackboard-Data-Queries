b1c251c90d44e627556c576bc0127a1500c95f49
Chanrattnak Mong <2023002mong@aupp.edu.kh>
2025-04-18 13:42:49 -0500

    add asssignment name attribute

--- filter-each-course-due-time/query.sql
+++ filter-each-course-due-time/query.sql
@@ -3,29 +3,18 @@ SELECT
     lc.name AS course_name,
     lc.design_mode,
     lt.start_date,
-    lg.due_time,
     lt.name AS term,
+    lg.name AS assignment_name,
+    lg.due_time,
     COUNT(DISTINCT p.id) AS instructor_count,
     LISTAGG(DISTINCT CONCAT(p.first_name, ' ', p.last_name), ', ') 
         WITHIN GROUP (ORDER BY CONCAT(p.first_name, ' ', p.last_name)) AS instructors,
     LISTAGG(DISTINCT p.email, ', ') 
         WITHIN GROUP (ORDER BY p.email) AS instructor_emails,
-    CASE
-        WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 2)
-        ELSE 'Unknown'
-    END AS institutional_hierarchy_level_1,
-    CASE
-        WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 3)
-        ELSE 'Unknown'
-    END AS institutional_hierarchy_level_2,
-    CASE
-        WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 4)
-        ELSE 'Unknown'
-    END AS institutional_hierarchy_level_3,
-    CASE
-        WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 5)
-        ELSE 'Unknown'
-    END AS institutional_hierarchy_level_4
+    CASE WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 2) ELSE 'Unknown' END AS institutional_hierarchy_level_1,
+    CASE WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 3) ELSE 'Unknown' END AS institutional_hierarchy_level_2,
+    CASE WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 4) ELSE 'Unknown' END AS institutional_hierarchy_level_3,
+    CASE WHEN ih.hierarchy_name_seq IS NOT NULL THEN SPLIT_PART(ih.hierarchy_name_seq, '||', 5) ELSE 'Unknown' END AS institutional_hierarchy_level_4
 FROM cdm_lms.course lc
 INNER JOIN cdm_lms.term lt ON lt.id = lc.term_id
 INNER JOIN cdm_lms.gradebook lg ON lg.course_id = lc.id
@@ -33,7 +22,7 @@ INNER JOIN cdm_lms.person_course lpc ON lpc.course_id = lc.id AND lpc.course_rol
 INNER JOIN cdm_lms.person p ON p.id = lpc.person_id
 INNER JOIN cdm_lms.institution_hierarchy_course ihc ON lc.id = ihc.course_id
 INNER JOIN cdm_lms.institution_hierarchy ih ON ih.id = ihc.institution_hierarchy_id
-WHERE course_name ='2025S_HHP440_A_Anatomical Kinesiology'
+WHERE lc.name = '2025S_HHP440_A_Anatomical Kinesiology'
 GROUP BY
-    lc.id, lc.name, lc.design_mode, lt.start_date, lt.name, ih.hierarchy_name_seq, lg.due_time
+    lc.id, lc.name, lc.design_mode, lt.start_date, lt.name, lg.name, lg.due_time, ih.hierarchy_name_seq
 ORDER BY lt.start_date, lc.name, lg.due_time ASC;
