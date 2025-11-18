e76eb2d8ffda411664b004118bd0e0c8605fa5f3
Chanrattnak Mong <mongchanrattnak@gmail.com>
2025-11-18 08:56:38 -0600

    Standardize instructor aggregation and add primary_ind filter to AIDA queries

--- ai-design-assistant-adoption/show-aida-usage-by-term/query.sql
+++ ai-design-assistant-adoption/show-aida-usage-by-term/query.sql
@@ -2,32 +2,32 @@
 with ai_summary as (
 select
     ifnull(ih.hierarchy_name_seq, 'No Node') as ih_node,
-    ifnull(lt.name, 'No Term') as term,
-    lci.course_id,
-    lc.name as course_name,
-    lc.course_number,
-    lci.id as course_item_id,
-    lci.item_type,
-    lci.name,
-    lci.stage,
-    lci.ai_status,
-    concat(lp.first_name,' ',lp.last_name) as creator,
-    lci.created_time,
-    lci.modified_time,
-    --datediff(second,lci.created_time,lci.modified_time) as modified_x_seconds_after_creation,
-    case when datediff(second,lci.created_time,lci.modified_time) > 10 then 'Modified' else 'Unmodified' end as modified_indicator
-from cdm_lms.course_item lci
-inner join cdm_lms.person lp
-    on lp.id = lci.person_id
-inner join cdm_lms.course lc
-    on lc.id = lci.course_id
+    ifnull(t.name, 'No Term') as term,
+    ci.course_id,
+    c.name as course_name,
+    c.course_number,
+    ci.id as course_item_id,
+    ci.item_type,
+    ci.name,
+    ci.stage,
+    ci.ai_status,
+    concat(p.first_name,' ',p.last_name) as creator,
+    ci.created_time,
+    ci.modified_time,
+    --datediff(second,ci.created_time,ci.modified_time) as modified_x_seconds_after_creation,
+    case when datediff(second,ci.created_time,ci.modified_time) > 10 then 'Modified' else 'Unmodified' end as modified_indicator
+from cdm_lms.course_item ci
+inner join cdm_lms.person p
+    on p.id = ci.person_id
+inner join cdm_lms.course c
+    on c.id = ci.course_id
 left join cdm_lms.institution_hierarchy_course ihc
-    on ihc.course_id = lc.id
+    on ihc.course_id = c.id
     And ihc.primary_ind = 1
 left join cdm_lms.institution_hierarchy ih
     on ihc.institution_hierarchy_id = ih.id
-left join cdm_lms.term lt
-    on lt.id = lc.term_id
+left join cdm_lms.term t
+    on t.id = c.term_id
 )
 
 select term, count(distinct course_id) as course_count, count(course_item_id) as item_count from ai_summary where ai_status = 'Y' group by 1 order by 3 desc;
